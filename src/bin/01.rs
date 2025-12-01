68fce7520a51a6597f3afeac713862252b8db3ed
Vince van Noort <vince.vannoort@channable.com>
2025-12-01 06:41:24 +0100

    feat(day 1): part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -34,7 +34,22 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut dial: i32 = 50;
+    let mut count: u64 = 0;
+    let inputs = parse_input(input);
+    for (direction, amount) in inputs {
+        for _ in 0..amount {
+            dial = match direction {
+                'L' => (dial - 1).rem_euclid(100),
+                'R' => (dial + 1).rem_euclid(100),
+                _ => panic!("unknown direction"),
+            };
+            if dial == 0 {
+                count += 1;
+            }
+        }
+    }
+    Some(count)
 }
 
 #[cfg(test)]
@@ -50,6 +65,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
