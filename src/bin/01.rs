28f3ed9a801905cc6f45562c9d78886642acd852
Vince van Noort <vince.vannoort@channable.com>
2025-12-01 06:37:54 +0100

    feat(day 1): part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,55 @@
+advent_of_code::solution!(1);
+
+fn parse_input(input: &str) -> Vec<(char, i32)> {
+    input
+        .lines()
+        .map(|line| {
+            (
+                line.chars().next().unwrap(),
+                line.chars()
+                    .skip(1)
+                    .collect::<String>()
+                    .parse::<i32>()
+                    .unwrap(),
+            )
+        })
+        .collect()
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut dial: i32 = 50;
+    let mut count: u64 = 0;
+    let inputs = parse_input(input);
+    for (direction, amount) in inputs {
+        dial = match direction {
+            'L' => (dial - amount).rem_euclid(100),
+            'R' => (dial + amount).rem_euclid(100),
+            _ => panic!("unknown direction"),
+        };
+        if dial == 0 {
+            count += 1;
+        }
+    }
+    Some(count)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
