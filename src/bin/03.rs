b3ceb83ace52361feb31ebb5160cf926724c2c32
Vince van Noort <vince.vannoort@channable.com>
2025-12-03 06:41:02 +0100

    feat(day 3): part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,6 +1,6 @@
-use std::cmp::max;
+use std::{cmp::max as max_cmp, option};
 
-use itertools::Itertools;
+use itertools::{Itertools, max as max_iter};
 
 advent_of_code::solution!(3);
 
@@ -17,27 +17,67 @@ fn parse(input: &str) -> Vec<Vec<u8>> {
 
 pub fn part_one(input: &str) -> Option<u64> {
     let inputs = parse(input);
-    let mut total = 0;
+    let mut result = 0;
     for input in inputs {
         let mut best = u64::MIN;
         for (index, num) in input.iter().enumerate() {
             let remaining_nums = &input[(index + 1)..];
             for remaining_num in remaining_nums {
-                best = max(best, (num * 10 + remaining_num) as u64);
+                best = max_cmp(best, (num * 10 + remaining_num) as u64);
             }
         }
-        total += best;
+        result += best;
     }
-    Some(total)
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let inputs = parse(input);
+    let mut result = 0;
+    for input in inputs {
+        let mut index = 0;
+        let mut inner_result = String::from("");
+        for num in (0..12).rev() {
+            let options = &input[index..(input.len() - num)];
+            let best_option = options.iter().max().unwrap();
+            let (best_index, _) = options.iter().find_position(|x| *x == best_option).unwrap();
+            inner_result.push_str(&best_option.to_string());
+            index = index + best_index + 1;
+        }
+        result += inner_result.parse::<u64>().unwrap()
+    }
+
+    Some(result)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_example1() {
+        assert_eq!(part_two("2311111111111"), Some(311111111111));
+    }
+
+    #[test]
+    fn test_example2() {
+        assert_eq!(part_two("987654321111111"), Some(987654321111));
+    }
+
+    #[test]
+    fn test_example3() {
+        assert_eq!(part_two("811111111111119"), Some(811111111119));
+    }
+
+    #[test]
+    fn test_example4() {
+        assert_eq!(part_two("234234234234278"), Some(434234234278));
+    }
+
+    #[test]
+    fn test_example5() {
+        assert_eq!(part_two("818181911112111"), Some(888911112111));
+    }
 
     #[test]
     fn test_part_one() {
@@ -48,6 +88,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
