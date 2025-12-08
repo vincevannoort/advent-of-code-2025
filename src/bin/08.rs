de07901c7b2a29664e1c35285c6d97c6ee587323
Vince van Noort <vince.vannoort@channable.com>
2025-12-08 21:24:54 +0100

    feat(day 8): part 1

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,5 +1,6 @@
 use itertools::Itertools;
-use std::collections::HashSet;
+use ordered_float::OrderedFloat;
+use std::{collections::HashSet, hash::Hash};
 
 advent_of_code::solution!(8);
 
@@ -24,63 +25,69 @@ fn euclidean_distance_3d(p1: (i32, i32, i32), p2: (i32, i32, i32)) -> f64 {
     (dx * dx + dy * dy + dz * dz).sqrt()
 }
 
+fn find_closest_pairs(
+    junction_boxes: &Vec<(i32, i32, i32)>,
+    paris: usize,
+) -> Vec<(&(i32, i32, i32), &(i32, i32, i32), f64)> {
+    junction_boxes
+        .iter()
+        .combinations(2)
+        .map(|pair| {
+            let a = pair[0];
+            let b = pair[1];
+            (a, b, euclidean_distance_3d(*a, *b))
+        })
+        .sorted_by_key(|(_, _, distance)| OrderedFloat(*distance))
+        .take(paris)
+        .collect_vec()
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let junction_boxes = parse(input);
-    let take_n_shortest_connections = if junction_boxes.len() == 1000 {
+    let pairs = if junction_boxes.len() == 1000 {
         1000
     } else {
         10
     };
 
-    let shortest_connections = junction_boxes
-        .iter()
-        .enumerate()
-        // find closest connection for every box
-        .map(|(i, junction_box_i)| {
-            let (closest_junction_box, distance) = junction_boxes
-                .iter()
-                .enumerate()
-                .filter(|(j, _)| i != *j)
-                .map(|(_, junction_box_j)| {
-                    (
-                        junction_box_j,
-                        euclidean_distance_3d(*junction_box_i, *junction_box_j),
-                    )
-                })
-                .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-                .unwrap();
-            (junction_box_i, closest_junction_box, distance)
-        })
-        // sort by distance
-        .sorted_by(|(_, _, distance_a), (_, _, distance_b)| {
-            distance_a.partial_cmp(distance_b).unwrap()
-        })
-        .take(take_n_shortest_connections)
-        // TODO: use less hacky approach here...
-        .map(|(a, b, d)| if a < b { (a, b, d) } else { (b, a, d) })
-        .unique_by(|(a, b, _)| (*a, *b))
-        .collect_vec();
+    let shortest_connections = find_closest_pairs(&junction_boxes, pairs);
 
     let mut circuits: Vec<HashSet<(i32, i32, i32)>> = vec![];
 
-    dbg!(&shortest_connections);
-
     for (a, b, _) in shortest_connections {
-        let circuit = circuits
+        let connected_circuits = circuits
             .iter_mut()
-            .find(|circuit| circuit.contains(a) || circuit.contains(b));
+            .enumerate()
+            .filter(|(_, circuit)| circuit.contains(a) || circuit.contains(b))
+            .map(|(index, _)| index)
+            .collect_vec();
 
         // create a new circuit
-        if circuit.is_none() {
+        if connected_circuits.is_empty() {
             let new_circuit = HashSet::from_iter(vec![*a, *b].into_iter());
             circuits.push(new_circuit);
             continue;
         }
 
         // extend existing circuit
-        if let Some(circuit) = circuit {
+        let first_circuit_index = *connected_circuits.first().unwrap();
+        if connected_circuits.len() == 1 {
+            let circuit = circuits.get_mut(first_circuit_index).unwrap();
             circuit.insert(*a);
             circuit.insert(*b);
+            continue;
+        }
+
+        // combine circuits
+        let second_circuit_index = *connected_circuits.last().unwrap();
+        if connected_circuits.len() == 2 {
+            let first_circuit = circuits.get(first_circuit_index).cloned().unwrap();
+            let second_circuit = circuits.get(second_circuit_index).cloned().unwrap();
+            circuits.retain(|circuit| *circuit != first_circuit && *circuit != second_circuit);
+            let mut combined_circuit: HashSet<(i32, i32, i32)> = HashSet::new();
+            combined_circuit.extend(&first_circuit);
+            combined_circuit.extend(&second_circuit);
+            circuits.push(combined_circuit);
         }
     }
 
@@ -89,11 +96,11 @@ pub fn part_one(input: &str) -> Option<u64> {
         .map(|circuit| circuit.len())
         .sorted()
         .rev()
-        .take(3)
         .collect_vec();
 
     let result = largest_circuits
         .into_iter()
+        .take(3)
         .reduce(|acc, val| acc * val)
         .unwrap();
 
@@ -109,44 +116,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_euclid_distance_3d() {
-        assert_eq!(
-            euclidean_distance_3d((1, 1, 1,), (2, 2, 2)),
-            1.7320508075688772f64
-        );
-    }
-
-    #[test]
     fn test_closest_connections() {
-        // 162,817,812 & 425,690,689 -> circuit of 2
-        // 162,817,812 & 431,825,988 -> circuit of 3
-        // 805,96,715 & 906,360,560 -> circuit of 3 + circuit of 2
-        // 431,825,988 & 425,690,689 -> ...
-    }
+        let junction_boxes = parse(&advent_of_code::template::read_file("examples", DAY));
+        let mut shortest_connections = find_closest_pairs(&junction_boxes, 10).into_iter();
 
-    #[test]
-    fn test_example1() {
-        let result = part_one(&advent_of_code::template::read_file_part(
-            "examples", DAY, 1,
-        ));
-        assert_eq!(result, Some(6));
+        let first = shortest_connections.next().unwrap();
+        assert_eq!((first.0, first.1), (&(162, 817, 812), &(425, 690, 689)));
+        let second = shortest_connections.next().unwrap();
+        assert_eq!((second.0, second.1), (&(162, 817, 812), &(431, 825, 988)));
+        let third = shortest_connections.next().unwrap();
+        assert_eq!((third.0, third.1), (&(906, 360, 560), &(805, 96, 715)));
+        let fourth = shortest_connections.next().unwrap();
+        assert_eq!((fourth.0, fourth.1), (&(431, 825, 988), &(425, 690, 689)));
     }
-
-    #[test]
-    fn test_example2() {
-        let result = part_one(&advent_of_code::template::read_file_part(
-            "examples", DAY, 2,
-        ));
-        assert_eq!(result, Some(6));
-    }
-
-    // #[test]
-    // fn test_example3() {
-    //     let result = part_one(&advent_of_code::template::read_file_part(
-    //         "examples", DAY, 3,
-    //     ));
-    //     assert_eq!(result, Some(6));
-    // }
 
     #[test]
     fn test_part_one() {
