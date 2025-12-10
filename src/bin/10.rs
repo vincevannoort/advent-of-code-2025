836402cf9500797a18749da80e49658d0910981b
Vince van Noort <vince.vannoort@channable.com>
2025-12-10 07:02:56 +0100

    feat(day 10): part 2 (unfinished)

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 use itertools::Itertools;
 
 advent_of_code::solution!(10);
@@ -6,6 +8,7 @@ advent_of_code::solution!(10);
 struct Machine {
     lights: Vec<char>,
     buttons: Vec<Vec<usize>>,
+    joltage: Vec<usize>,
 }
 
 fn parse(input: &str) -> Vec<Machine> {
@@ -34,7 +37,20 @@ fn parse(input: &str) -> Vec<Machine> {
                         .collect_vec()
                 })
                 .collect_vec();
-            Machine { lights, buttons }
+            let joltage = line
+                .split_once(" {")
+                .unwrap()
+                .1
+                .replace("}", "")
+                .split(",")
+                .map(|number| number.parse().unwrap())
+                .collect_vec();
+
+            Machine {
+                lights,
+                buttons,
+                joltage,
+            }
         })
         .collect();
 
@@ -80,8 +96,43 @@ pub fn part_one(input: &str) -> Option<u64> {
     )
 }
 
+fn toggle_buttons_matches_joltage_goal(joltage: &Vec<usize>, buttons: &Vec<&Vec<usize>>) -> bool {
+    // let goal: Vec<bool> = light.iter().map(|c| *c == '#').collect_vec();
+    let mut start: Vec<usize> = joltage.iter().map(|_| 0).collect_vec();
+    for button in buttons {
+        for number in button.iter() {
+            start[*number] += 1;
+        }
+    }
+    *joltage == start
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let machines = parse(input);
+    Some(
+        machines
+            .iter()
+            .map(|machine| {
+                let mut presses = 1;
+                loop {
+                    let combination = machine
+                        .buttons
+                        .iter()
+                        .combinations_with_replacement(presses)
+                        // TODO: do something smart here, we know that we need a lot of presses
+                        .find(|buttons| {
+                            toggle_buttons_matches_joltage_goal(&machine.joltage, buttons)
+                        });
+
+                    if let Some(combination) = combination {
+                        return combination.len() as u64;
+                    } else {
+                        presses += 1;
+                    }
+                }
+            })
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -97,6 +148,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(33));
     }
 }
